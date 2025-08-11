aad9938f566ab1e88e21712e2b50598667b7dd99
Julius Adebowale <juadebgabriel@gmail>
2025-08-11 16:29:02 +0100

    Added

---
+++ greeting.py
@@ -0,0 +1,10 @@
+print("Hello")
+
+name = input("What's your name")
+print(f"Hello, {name}! Welcome to python")
+
+def greet(name="World"):
+    return f"hello, {name}!"
+
+print(greet())
+print(greet("Python Developer"))
